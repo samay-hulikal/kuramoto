a490e7ab990680b1df11490c1ba2f1592e3d7a7e
samay-peleglab-dell <samay-peleglab-dell>
2026-01-29 16:58:20 -0700

    Add ipython to dependencies and began learning

---
+++ src/basics.py
@@ -0,0 +1,9 @@
+import numpy as np
+python_list = [1,2,3,4]
+
+numpy_array = np.array([1,2,3])
+
+print("Python lst:", python_list)
+print("Numpy lst:", numpy_array)
+print("Type of list", type(python_list))
+print("Type of list", type(numpy_array))
