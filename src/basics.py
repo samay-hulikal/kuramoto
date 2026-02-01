e5cc2d9ee8b034d1c0d724816e45a8be72d86e90
Samay's Mac mini <samay@Samays-Mac-mini>
2026-02-01 13:11:06 -0700

    Checking behavior of gitsigns

--- src/basics.py
+++ src/basics.py
@@ -1,10 +1,9 @@
 import numpy as np
-python_list = [1,2,3,4]
 
-numpy_array = np.array([1,2,3])
+# arange, linspace, etc. 
+x = np.arange(3,7.2, 2)
+print("arange", x)
 
-print("Python lst:", python_list)
-print("Numpy lst:", numpy_array)
-print("Type of list", type(python_list))
-print("Type of list", type(numpy_array))
-print(np.sin(0))
+lns = np.linspace(0,1,10)
+print("arange", )
+print()
