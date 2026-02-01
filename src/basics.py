ccd42a513198b7a79d24b37871fd619b8e6467c9
Samay's Mac mini <samay@Samays-Mac-mini>
2026-02-01 16:36:17 -0700

    More shit

--- src/basics.py
+++ src/basics.py
@@ -5,5 +5,16 @@ x = np.arange(3,7.2, 2)
 print("arange", x)
 
 lns = np.linspace(0,1,10)
-print("arange", )
+print("arange", lns[6:9])
 print()
+
+arr = np.array([[1, 2, 4],
+                [4, 5, 6]])
+result = arr**3 + 2*arr + 2
+print(result)
+
+rand = np.random.uniform(-1, 1, 10)
+print(rand)
+
+randgauss = np.random.normal(1,1,10)
+print(randgauss, len(randgauss))
